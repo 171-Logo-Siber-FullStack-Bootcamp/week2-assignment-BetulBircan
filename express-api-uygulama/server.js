e44126fb2a41fb7b08041b9a950f54ca1bad4e8d
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-02-13 11:12:41 +0300

    user posts ve todos verilerini getirmek için gereken dosyalar oluşturuldu.

---
+++ express-api-uygulama/server.js
@@ -0,0 +1,3 @@
+//Main yani ana dosyamız server.js
+//Biz burada modüler bir yapı kurmayı amaçladık. Amaç ana dosuyada kodların az olması. Diğer örneklerde de oluğu gibi getirmek istediğimiz posts,users ve todos verilerini src kaynal klasöründen server.js e require ile ekleyeceğiz.
+
