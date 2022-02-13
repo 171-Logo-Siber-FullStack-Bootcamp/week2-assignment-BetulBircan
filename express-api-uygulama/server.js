97f8bf80a74d2f1e197fc3bc2dad940371cd9aec
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-02-13 11:18:58 +0300

    post verilerini getirmek için yapılan işlemler

--- express-api-uygulama/server.js
+++ express-api-uygulama/server.js
@@ -1,3 +1,27 @@
 //Main yani ana dosyamız server.js
 //Biz burada modüler bir yapı kurmayı amaçladık. Amaç ana dosuyada kodların az olması. Diğer örneklerde de oluğu gibi getirmek istediğimiz posts,users ve todos verilerini src kaynal klasöründen server.js e require ile ekleyeceğiz.
 
+//express kütüphanesini sayfaya ekleme
+const express = require("express")
+//src/controller klasöründeki posts-controller dosyasını buraya ekledik
+const postController = require("./src/controllers/posts-controller.js")  
+
+//uygulamayı oluşturma
+const app = express()   
+
+
+//get ile postları çağırma
+app.get("/posts", (req, res ) => {
+    //controller çağrılması
+    var posts = postController.getAllPosts()
+    //data return
+    res.send(posts)
+})
+
+//post taki post/3 deki postu getirme
+app.get("/posts/:postId", (req, res) => {
+    var post = postController.getByPostId(req.params.postId)
+    res.send(post)
+})
+
+app.listen(6600)   //server ı dinleme
