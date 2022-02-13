25515bff6ecb434647eaa8a0dfc3482cbbfe8363
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-02-13 10:55:35 +0300

    express-api/server.js ve data/users.json ve post.json verileri oluşturuldu

---
+++ express-api/server.js
@@ -0,0 +1,19 @@
+//Express bir web frameworkudur.Bir restfulapi ya da web sayfası geliştirme altyapısıdır.
+
+/*express ile server oluşturma:
+
+//express paketi çağırma
+const express = require("express")
+//expressle uygulama oluştu
+const app = express()
+//get çağrıları için yazılan kodlar
+app.get("/",(req,res) => {    "/" kök dizin
+    res.send("Server Çalıştı")
+})
+
+//Port Oluşturma
+const PORT = 5000
+//App i dinleme(server ı )
+app.listen(PORT)
+*/
+
